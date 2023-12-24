6eb5d06ddf0e07235922804c16a8336b21ba79e0
maikonmuniz <maikon.muniz@yahoo.com.br>
2023-12-23 23:59:56 -0300

    feat: create entity product

---
+++ src/main/java/com/product/crud/domain/product/Product.java
@@ -0,0 +1,24 @@
+package com.product.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name="product")
+@Entity(name="product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+}
